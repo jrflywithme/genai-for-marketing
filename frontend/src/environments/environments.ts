757518bb10d3cd6fa45222f649e64fc2ca017cae
Jitendra Rathore <jrflywithme@gmail.com>
2025-01-24 23:32:25 +0530

    Update environments.ts
    remove api key

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,14 +1,14 @@
 // Replace with you configuration for the Firebase Hosting
 export const environment = {
-    apiUrl: "https://genai-for-marketing-backend-apis-i5aejgsyra-uc.a.run.app/marketing-api",
-    dialogflowCxAgentId: "8da36323-2a0c-4bcb-98ce-315e3e6730f2",
+    apiUrl: "",
+    dialogflowCxAgentId: "",
     firebaseConfig: {
-        apiKey: "AIzaSyAlyQ2xLgqTnIZQ1QzvDwUllGdVukxGDrI",
-        authDomain: "ai-agent-for-marketing.firebaseapp.com",
-        projectId: "ai-agent-for-marketing",
-        storageBucket: "ai-agent-for-marketing.firebasestorage.app",
-        messagingSenderId: "1069569248391",
-        appId: "1:1069569248391:web:2b90670e731b5179cb3c22",
-        measurementId: "G-D1GN35VN2D"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
       }
 }
